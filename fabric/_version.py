f3a167cf9719ef383320b222e532515b5d2f087c
Romanin <60302782+romanin-rf@users.noreply.github.com>
2025-01-16 16:26:35 +0200

    Patch №1

--- fabric/_version.py
+++ fabric/_version.py
@@ -1,2 +1,2 @@
-__version_info__ = (3, 2, 2)
-__version__ = ".".join(map(str, __version_info__))
+__version_info__: tuple[int, int, int]  = (1, 0, 322)
+__version__: str                        = ".".join(map(str, __version_info__))
